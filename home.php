fd144ea0e49598d21d95c33e5c679068d3474a55
Milan9135 <Milpower0@gmail.com>
2024-01-15 15:35:12 +0100

    styles

--- home.php
+++ home.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JDM Premium Verhuur</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles/Don.css">
 </head>
 
 <body>
